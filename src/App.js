fe0f40540479978a5399b52fbcb2d562bb75b853
Evelyn Navazio <graueve@hotmail.com>
2021-12-12 17:12:07 -0300

    carousel con reactstrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Carousele from './components/Carousel/Carousele'
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Carousele/>
+      
       <ItemListContainer greeting='Titulo provisional' />
     </div>
   );
